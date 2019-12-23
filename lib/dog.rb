89a405385592d42d3eaae28f67b6901707a9c1db
Zachary Sciscente <zsciscente@gmail.com>
2019-12-23 03:02:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,6 @@
 class Dog
+  
+  
 end
 
 fido = Dog.new
@@ -10,3 +12,20 @@ end
 
 
 fido.bark
+
+class Dog
+end
+
+def bark
+  puts "Woof!"
+end
+
+def sit 
+  puts "The Dog is sitting."
+end
+
+fido = Dog.new
+
+fido.bark
+
+fido.sit
