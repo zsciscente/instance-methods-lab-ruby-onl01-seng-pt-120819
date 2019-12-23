d3b3d83468ea627c983c6a4d0ee21ea038414e52
Zachary Sciscente <zsciscente@gmail.com>
2019-12-23 02:56:38 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+end
+
+def bark
+  puts "Woof!"
+end
+
+def sit 
+  puts "The Dog is sitting."
+end
+
+fido = Dog.new
+
+fido.bark
+
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+end
+
+def talk
+  puts "Hello World!"
+end
+
+def walk
+  puts "The Person is walking."
+end
+
+ada = Person.new
+
+ada.talk
+
+ada.walk
+
